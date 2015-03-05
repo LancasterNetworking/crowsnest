55341bd108e185d1bfa3c7cb7827531000e226e2
Sean Baildon <sean@baildon.co>
2015-03-05 14:51:34 +0000

    remove timer from session object
    no need for timer anymore

--- watchtower/node/session.py
+++ watchtower/node/session.py
@@ -1,14 +1,5 @@
-from threading import Timer, Thread
-
 class Session(object):
-
 	def __init__(self, mpd, start_time):
 		self.mpd = mpd
 		self.start_time = start_time
 		self.elapsed_time = 0
-		#self.timer()
-
-	def timer(self):
-		self.elapsed_time += 1
-		t = Timer(1.0, self.timer)
-		t.start()
