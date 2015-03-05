a235cf024ac6e5276f8c9c268dca4b1c118d7d63
afarshad <arsham.farshad@gmail.com>
2015-03-05 19:42:35 +0000

    "change the library name in the database"

---
+++ crowsnest/lib/database.py
@@ -0,0 +1,6 @@
+from crowsnest import config
+from pymongo import Connection
+
+def open_connection():
+	connection = Connection(config.database['host'], int(config.database['port']))
+	return connection[config.database['name']]
