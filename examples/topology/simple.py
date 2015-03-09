628831e55570edc4468b2f99d6d7340edafebb2f
Sean Baildon <sean@baildon.co>
2015-03-09 14:54:52 +0000

    inline code style with main program, remove unnecessary prints

--- examples/topology/simple.py
+++ examples/topology/simple.py
@@ -25,63 +25,61 @@ from mininet.node import Node
 from mininet.topolib import TreeTopo
 from mininet.link import Link
 
-def TreeNet( depth=1, fanout=2, **kwargs ):
+def TreeNet(depth=1, fanout=2, **kwargs):
     "Convenience function for creating tree networks."
-    topo = TreeTopo( depth, fanout )
-    return Mininet( topo=topo, controller=lambda name: None, xterms=False, **kwargs )
+    topo = TreeTopo(depth, fanout)
+    return Mininet(topo=topo, controller=lambda name: None, xterms=False, **kwargs)
 
-def connectToRootNS( network, switch, ip, routes ):
+def connectToRootNS(network, switch, ip, routes):
     """Connect hosts to root namespace via switch. Starts network.
       network: Mininet() network object
       switch: switch to connect to root namespace
       ip: IP address for root namespace node
       routes: host networks to route to"""
     # Create a node in root namespace and link to switch 0
-    root = Node( 'root', inNamespace=False )
-    intf = Link( root, switch ).intf1
-    root.setIP( ip, intf=intf )
+    root = Node('root', inNamespace=False)
+    intf = Link(root, switch).intf1
+    root.setIP(ip, intf=intf)
     # Start network that now includes link to root namespace
     network.start()
     # Add routes from root ns to hosts
     for route in routes:
-        root.cmd( 'route add -net ' + route + ' dev ' + str( intf ) )
+        root.cmd('route add -net ' + route + ' dev ' + str(intf))
 
-def sshd( network, cmd='/usr/sbin/sshd', opts='-D',
-          ip='10.123.123.1/32', routes=None, switch=None ):
+def sshd(network, cmd='/usr/sbin/sshd', opts='-D',
+          ip='10.123.123.1/32', routes=None, switch=None):
     """Start a network, connect it to root ns, and run sshd on all hosts.
        ip: root-eth0 IP address in root namespace (10.123.123.1/32)
        routes: Mininet host networks to route to (10.0/24)
        switch: Mininet switch to connect to root namespace (s1)"""
     if not switch:
-        switch = network[ 's1' ]  # switch to use
+        switch = network['s1']  # switch to use
     if not routes:
-        routes = [ '10.0.0.0/24' ]
-    connectToRootNS( network, switch, ip, routes )
-    switch.cmd( 'ovs-vsctl set-controller s1 tcp:10.123.123.1') # set controller to VM host in root namespace
+        routes = ['10.0.0.0/24']
+    connectToRootNS(network, switch, ip, routes)
+    switch.cmd('ovs-vsctl set-controller s1 tcp:10.123.123.1') # set controller to VM host in root namespace
     for host in network.hosts:
-        host.cmd( cmd + ' ' + opts + '&' )
-    print network.getNodeByName('h1').cmd('sudo killall mongod > /dev/null 2>&1')
-    print network.getNodeByName('h1').cmd('sudo mkdir -p /data/db/')
-    print network.getNodeByName('h1').cmd('sudo rm -f /data/db/mongod.lock')
-    print network.getNodeByName('h1').cmd('mongod --bind_ip=10.0.0.1 --fork --logpath /var/log/mongo.log') # run mongod on h1
-    print
+        host.cmd(cmd + ' ' + opts + '&')
+    network.getNodeByName('h1').cmd('sudo killall mongod > /dev/null 2>&1')
+    network.getNodeByName('h1').cmd('sudo mkdir -p /data/db/')
+    network.getNodeByName('h1').cmd('sudo rm -f /data/db/mongod.lock')
+    network.getNodeByName('h1').cmd('mongod --bind_ip=10.0.0.1 --fork --logpath /var/log/mongo.log') # run mongod on h1
+    
     print "*** Hosts are running sshd at the following addresses:"
-    print
     for host in network.hosts:
         print host.name, host.IP()
-    print
+        
     print "*** Type 'exit' or control-D to shut down network"
-    CLI( network )
+    CLI(network)
     for host in network.hosts:
-        host.cmd( 'kill %' + cmd )
-    network.getNodeByName('h1').cmd( 'sudo killall mongod' ) # run mongod on h1
+        host.cmd('kill %' + cmd)
+    network.getNodeByName('h1').cmd('sudo killall mongod') # run mongod on h1
     network.stop()
 
 if __name__ == '__main__':
-    lg.setLogLevel( 'info')
-    net = TreeNet( depth=1, fanout=4 )
+    lg.setLogLevel('info')
+    net = TreeNet(depth=1, fanout=4)
     # get sshd args from the command line or use default args
     # useDNS=no -u0 to avoid reverse DNS lookup timeout
-    opts = ' '.join( sys.argv[ 1: ] ) if len( sys.argv ) > 1 else (
-        '-D -o UseDNS=no -u0' )
-    sshd( net, opts=opts )
+    opts = ' '.join(sys.argv[1:]) if len(sys.argv) > 1 else ('-D -o UseDNS=no -u0')
+    sshd(net, opts=opts)
