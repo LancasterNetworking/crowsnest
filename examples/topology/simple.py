d9774b9b5237e8c7975ee2f6c0aacaae7920ed85
Sean Baildon <sean@baildon.co>
2015-03-09 14:24:41 +0000

    mininet topology fixes
    create a directory that may not exist, and fork the mongod process properly instead of sending to shell background

--- examples/topology/simple.py
+++ examples/topology/simple.py
@@ -60,9 +60,10 @@ def sshd( network, cmd='/usr/sbin/sshd', opts='-D',
     switch.cmd( 'ovs-vsctl set-controller s1 tcp:10.123.123.1') # set controller to VM host in root namespace
     for host in network.hosts:
         host.cmd( cmd + ' ' + opts + '&' )
-    print network.getNodeByName('h1').cmd( 'sudo killall mongod > /dev/null 2>&1' )
-    print network.getNodeByName('h1').cmd( 'sudo rm -f /data/db/mongod.lock' )
-    print network.getNodeByName('h1').cmd( 'mongod --bind_ip=10.0.0.1 &' ) # run mongod on h1
+    print network.getNodeByName('h1').cmd('sudo killall mongod > /dev/null 2>&1')
+    print network.getNodeByName('h1').cmd('sudo mkdir -p /data/db/')
+    print network.getNodeByName('h1').cmd('sudo rm -f /data/db/mongod.lock')
+    print network.getNodeByName('h1').cmd('mongod --bind_ip=10.0.0.1 --fork --logpath /var/log/mongo.log') # run mongod on h1
     print
     print "*** Hosts are running sshd at the following addresses:"
     print
